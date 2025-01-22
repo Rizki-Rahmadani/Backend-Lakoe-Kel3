849b0516b91e8339424ece597f971047a35ad9e7
yudisaqsha <yudis.m.aqsha@gmail.com>
2025-01-22 13:31:38 +0700

    chore: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,60 @@
-import  express, { Request, Response }from "express"
+import express, { Request, Response } from 'express';
 export const app = express();
 import dotenv from 'dotenv';
-import swaggerUi from "swagger-ui-express";
-import swaggerDoc from "./swagger/swagger-output.json";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from './swagger/swagger-output.json';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import router from "./routes/v1/index.route";
+import router from './routes/v1/index.route';
 
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 const PORT = process.env.PORT;
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, {
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDoc, {
     explorer: true,
     swaggerOptions: {
-        persisAuthorization: true,
-        displayRequestDuration: true
-    }
-}))
+      persisAuthorization: true,
+      displayRequestDuration: true,
+    },
+  }),
+);
 
 app.use('/api', router);
 app.get('/', (request: Request, response: Response) => {
-    response.status(200).send('Hello World');
-  });
-app.get('/users', (req, res) => {
-    /* #swagger.tags = ['Users'] */
-})
+  response.status(200).send('Hello World');
+});
 
-app.get('/cart', (req, res)=>{
-    /*
+app.get('/cart', (req, res) => {
+  /*
         #swagger.tags = ['carts']
         #swagger.description = "to display users items"
     */
-})
+});
 
 app.get('/invoice', (req, res) => {
-    /* 
+  /* 
         #swagger.tags ['invoice']
         #swagger.description = "to get user invoice history"    
     */
-
-    /*
+  /*
         #swagger.parameters['obj] = {
             in: 'body',
             description: 'user invoice.',
             required: false
         }
     */
-})
+});
 
-app.get('/stores', (req, res)=>{
-    /*
+app.get('/stores', (req, res) => {
+  /*
         #swagger.tags["stores"]
         #swagger.description = "to display all stores"
     */
-})
+});
 
 app
   .listen(PORT, () => {
