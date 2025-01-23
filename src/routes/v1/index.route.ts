f0a6d8cdc87602c79c74fe54a7623705d16cdb5c
yudisaqsha <yudis.m.aqsha@gmail.com>
2025-01-23 13:20:14 +0700

    chore: rewriting index route

--- src/routes/v1/index.route.ts
+++ src/routes/v1/index.route.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import authRoute from './auth.route';
 import roleRoute from './role.route';
 import profileRoute from './profile.route';
-import app_message from './message.route';
-import app_store from './store.route';
+// import app_message from './message.route';
+// import app_store from './store.route';
 import { authentication } from '../../middlewares/authmiddleware';
 import app_product from './product.route';
 
@@ -17,10 +17,10 @@ router.use('/role', roleRoute);
 router.use('/auth', authRoute);
 router.use('/profile', profileRoute);
 
-router.use('/message', authentication, app_message);
-router.use('/stores', app_store);
-router.use('/product', app_product);
+// router.use('/message', authentication, app_message);
+// router.use('/stores', app_store);
 router.use('/category', categoryRoute);
+
 router.use('/carts', cartsRoute);
 router.use('/cart-items', cartsitemsRoute);
 
