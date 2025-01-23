2877639bb4aca5d26836687187c73c8e2079b17f
IamSom3On3 :) <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-01-23 08:58:54 +0700

    updated cloud upload index and product route

--- src/routes/v1/index.route.ts
+++ src/routes/v1/index.route.ts
@@ -2,9 +2,16 @@ import express from 'express';
 import authRoute from './auth.route';
 import roleRoute from './role.route';
 import profileRoute from './profile.route';
+import app_message from './message.route';
+import app_store from './store.route';
+import { authentication } from '../../middlewares/authmiddleware';
+import app_product from './product.route';
 const router = express.Router();
 
-router.use('/role', roleRoute)
+router.use('/role', roleRoute);
 router.use('/auth', authRoute);
-router.use('/profile', profileRoute)
+router.use('/profile', profileRoute);
+router.use('/message', authentication, app_message);
+router.use('/stores', app_store);
+router.use('/product', app_product);
 export default router;
