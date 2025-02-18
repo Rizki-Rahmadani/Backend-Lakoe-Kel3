1579d54fb0d75f42e9b1efe0287afe10736d5b22
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-02-18 08:59:49 +0700

    chore: add weight product

--- src/routes/v1/webhook.route.ts
+++ src/routes/v1/webhook.route.ts
@@ -4,3 +4,4 @@ import * as webhook from '../../controllers/webhook.controller';
 const webhookRoute = express.Router();
 webhookRoute.post('/biteship/tracking', webhook.BiteshipTracking);
 webhookRoute.post('/midtrans', webhook.Midtrans);
+export default webhookRoute;
