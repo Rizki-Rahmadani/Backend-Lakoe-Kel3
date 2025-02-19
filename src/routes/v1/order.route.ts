efe5b926680fca2f773e94bf62bb1a9f07d03743
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-02-19 11:58:22 +0700

    chore: fixing bugs

--- src/routes/v1/order.route.ts
+++ src/routes/v1/order.route.ts
@@ -3,7 +3,6 @@ import * as orders from '../../controllers/order.controller';
 import { authentication } from '../../middlewares/authmiddleware';
 const orderRoute = express.Router();
 
-orderRoute.post('/add-order', orders.createOrder);
 orderRoute.put('/update-order', orders.updateOrder);
 orderRoute.post('/confirm/:id', orders.confirmDraftOrder);
 orderRoute.post('/draft-order', orders.createDraftOrder);
