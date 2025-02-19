8ceebc9cecbdeb39f003d13374f8889d15670219
mujtahidul08 <mujahaq@gmail.com>
2025-02-19 09:12:57 +0700

    add retrive order, webhook controller and schema prisma

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -49,7 +49,7 @@ export async function BiteshipTracking(req: Request, res: Response) {
 
           // Update status di database jika ada perubahan
           await prisma.orders.update({
-            where: { order_id: event.order_id },
+            where: { biteship_order_id: event.order_id },
             data: { status: statusOrder },
           });
 
@@ -97,7 +97,6 @@ export const Midtrans = async (req: Request, res: Response) => {
     );
 
     console.log('response dari midtrans: ', midtransResponse);
-
     const result = midtransResponse.data;
     const transactionStatus = result.transaction_status;
     const fraudStatus = result.fraud_status;
@@ -135,30 +134,18 @@ export const Midtrans = async (req: Request, res: Response) => {
       );
 
       // confirm order to biteship if is paid
-      const draftOrdersId = '2131241';
-      const orderBiteship = await axios.post(
-        `${apiURL}/order/confirm/${draftOrdersId}`,
-      );
+      // const draftOrdersId = '2131241';
+      // const orderBiteship = await axios.post(
+      //   `${apiURL}/order/confirm/${draftOrdersId}`,
+      // );
 
-      //menambahkan order ke database
-      const dbOrders = await prisma.orders.create({
-        data: {
-          order_id: midtransResponse.data.id,
-          receiver_name: '',
-          status: 'Pesanan Baru',
-          biteship_order_id: orderBiteship.data.id,
-          // midtrans_order_id:midtransResponse.data.id,
-          storeId: '',
-          locationId: '',
-        },
-      });
-
-      // order_id      String   @unique
-      // receiver_name String
-      // status        String
-      // biteship_order_id String?
-      // storeId       String
-      // locationId    String
+      //update order Status ke database
+      // const dbOrders = await prisma.orders.update({
+      //   where:{ midtrans_order_id: midtransResponse.data.},
+      //   data: {
+      //     status: 'Pesanan Baru',
+      //   },
+      // });
 
       const invoice_history_response = await axios.post(
         apiURL + '/invoice-history/create-invoice-history',
@@ -217,20 +204,20 @@ export const Midtrans = async (req: Request, res: Response) => {
         },
       );
 
-      //create order status failed
-      //  const dbOrders = await prisma.orders.update({
-      //   data:{
-      //     order_id:midtransResponse.data.id,
-      //     receiver_name:"",
-      //     status:"Pesanan Baru",
-      //     biteship_order_id:orderBiteship.data.id,
-      //     // midtrans_order_id:midtransResponse.data.id,
-      //     storeId:"",
-      //     locationId:"",
-
-      //   }
-      // }
-      // )
+      //=====================================
+      // confirm order to biteship if is paid
+      // const draftOrdersId = '2131241';
+      // const orderBiteship = await axios.post(
+      //   `${apiURL}/order/confirm/${draftOrdersId}`,
+      // );
+      //update order Status ke database
+      // const dbOrders = await prisma.orders.update({
+      //   where:{ midtrans_order_id: midtransResponse.data.},
+      //   data: {
+      //     status: 'Dibatalkan',
+      //   },
+      // });
+      //=====================================
 
       const invoice_history_response = await axios.post(
         apiURL + '/invoice-history/create-invoice-history',
@@ -275,24 +262,20 @@ export const Midtrans = async (req: Request, res: Response) => {
         },
       );
 
+      //=====================================
       // confirm order to biteship if is paid
-      const draftOrdersId = '2131241';
-      const orderBiteship = await axios.post(
-        `${apiURL}/order/confirm/${draftOrdersId}`,
-      );
-
-      //menambahkan order ke database
-      const dbOrders = await prisma.orders.create({
-        data: {
-          order_id: midtransResponse.data.id,
-          receiver_name: '',
-          status: 'Pesanan Baru',
-          biteship_order_id: orderBiteship.data.id,
-          // midtrans_order_id:midtransResponse.data.id,
-          storeId: '',
-          locationId: '',
-        },
-      });
+      // const draftOrdersId = '2131241';
+      // const orderBiteship = await axios.post(
+      //   `${apiURL}/order/confirm/${draftOrdersId}`,
+      // );
+      //update order Status ke database
+      // const dbOrders = await prisma.orders.update({
+      //   where:{ midtrans_order_id: midtransResponse.data.},
+      //   data: {
+      //     status: 'Pesanan Baru',
+      //   },
+      // });
+      //=====================================
 
       // Create payment record
       await axios.post(
@@ -392,338 +375,3 @@ export const Midtrans = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
-
-// import { PrismaClient } from '@prisma/client';
-// import axios from 'axios';
-// import { Request, Response } from 'express';
-// const prisma = new PrismaClient();
-
-// export async function BiteshipTracking(req: Request, res: Response) {
-//   const event = req.body;
-//   console.log('Webhook Biteship diterima:', event);
-
-//   // Cek apakah ada perubahan status pada order tertentu
-//   if (event.order_id && event.status) {
-//     console.log(`Order ID: ${event.order_id}`);
-//     console.log(`Status Order: ${event.status}`);
-
-//     // Map status dari Biteship ke status yang diinginkan
-//     let statusOrder = event.status;
-//     switch (event.status) {
-//       case 'allocated':
-//         statusOrder = 'siap dikirim';
-//         break;
-//       case 'droppingOff':
-//         statusOrder = 'dalam pengiriman';
-//         break;
-//       case 'delivered':
-//         statusOrder = 'pesanan selesai';
-//         break;
-//       case 'cancelled':
-//         statusOrder = 'dibatalkan';
-//         break;
-//       default:
-//         statusOrder = event.status; // Jika tidak ada yang cocok, gunakan status asli
-//     }
-
-//     console.log(`Status Order setelah diubah: ${statusOrder}`);
-
-//     // Cek status lama dari database
-//     try {
-//       const existingOrder = await prisma.orders.findUnique({
-//         where: { order_id: event.order_id },
-//         select: { status: true },
-//       });
-
-//       if (existingOrder) {
-//         // Bandingkan status lama dengan status baru
-//         if (existingOrder.status !== statusOrder) {
-//           console.log(
-//             `Status berubah dari '${existingOrder.status}' menjadi '${statusOrder}'`,
-//           );
-
-//           // Update status di database jika ada perubahan
-//           await prisma.orders.update({
-//             where: { order_id: event.order_id },
-//             data: { status: statusOrder },
-//           });
-
-//           console.log(
-//             `Status order ${event.order_id} berhasil diperbarui menjadi ${statusOrder}`,
-//           );
-//         } else {
-//           console.log(
-//             `Tidak ada perubahan status untuk Order ID ${event.order_id}`,
-//           );
-//         }
-//       } else {
-//         console.log(`Order ID ${event.order_id} tidak ditemukan di database`);
-//       }
-//     } catch (error) {
-//       console.error('Gagal memperbarui status order:', error);
-//     }
-//   }
-
-//   // Biteship mengharapkan response 200 untuk menandakan webhook berhasil diproses
-//   res.status(200).send('Webhook received');
-// }
-
-// const apiURL = 'http://localhost:3000/api'; // Replace with your actual API URL
-// // const token = 'YOUR_AUTH_TOKEN'; // Replace with actual token if required
-
-// export async function Midtrans(req: Request, res: Response) {
-//   try {
-//     const data = req?.body;
-//     console.log('Midtrans Webhook Data:', data);
-
-//     // Verify the notification with Midtrans
-//     const midtransResponse = await axios.get(
-//       `https://api.sandbox.midtrans.com/v2/${data.order_id}/status`,
-//       {
-//         headers: {
-//           Authorization:
-//             'Basic ' +
-//             Buffer.from('SB-Mid-server-HTB5WCLALgagfSdtqzJVMkgB').toString(
-//               'base64',
-//             ),
-//           'Content-Type': 'application/json',
-//         },
-//       },
-//     );
-
-//     console.log('response dari midtrans: ', midtransResponse);
-
-//     const result = midtransResponse.data;
-//     const transactionStatus = result.transaction_status;
-//     const fraudStatus = result.fraud_status;
-
-//     if (transactionStatus === 'capture' && fraudStatus === 'accept') {
-//       console.log(`Payment for order ${data.order_id} is captured.`);
-//       const service_charge = (800000 * 2 * 1) / 100;
-//       const invoice_response = await axios.post(
-//         apiURL + '/invoice/create-invoice',
-//         {
-//           status: 'success',
-//           prices: 800000 * 2,
-//           service_charge: service_charge,
-//           receiver_city: 'jkt',
-//           receiver_province: 'jkt',
-//           receiver_subDistrict: 'jkt',
-//           receiver_district: 'jkt',
-//           receiver_phone: '092131322',
-//           receiver_name: 'sam',
-//           receiver_postalCode: '12440',
-//           receiver_detailAddress: 'ehiuewuhwuiehrweur',
-//           receiver_email: 'sam@mail.com',
-//           // cartsId: 'cdqdwir39232',
-//           userId: 'cm74hxlxy0009tsd4lzhwhwmv',
-//           order_id: data.order_id,
-//           // paymentsId: 'joewjfiewjfiwf',
-//           // courierId: 'wqeijeiqejei',
-//         },
-//         {
-//           headers: {
-//             // Authorization: `bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-
-//       const invoice_history_response = await axios.post(
-//         apiURL + '/invoice-history/create-invoice-history',
-//         {
-//           invoice_id: invoice_response.data.invoice_created.id,
-//         },
-//       );
-//       console.log('invoice_history created: ', invoice_history_response.data);
-//       await axios.post(
-//         `${apiURL}/payment/create-payment`,
-//         {
-//           bank: result.va_numbers?.[0]?.bank || 'unknown',
-//           gross_amount: Number(result.gross_amount),
-//           status_code: result.status_code,
-//           midtrans_transaction_id: result.transaction_id,
-//           invoicesId: invoice_response.data.invoice_created.id,
-//         },
-//         {
-//           headers: {
-//             // Authorization: `Bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-//     }
-
-//     if (transactionStatus === 'deny') {
-//       console.log(`Payment for order ${data.order_id} is deny.`);
-//       const service_charge = (800000 * 2 * 1) / 100;
-//       const invoice_response = await axios.post(
-//         apiURL + '/invoice/create-invoice',
-//         {
-//           status: 'success',
-//           prices: 800000 * 2,
-//           service_charge: service_charge,
-//           receiver_city: 'jkt',
-//           receiver_province: 'jkt',
-//           receiver_subDistrict: 'jkt',
-//           receiver_district: 'jkt',
-//           receiver_phone: '092131322',
-//           receiver_name: 'sam',
-//           receiver_postalCode: '12440',
-//           receiver_detailAddress: 'ehiuewuhwuiehrweur',
-//           receiver_email: 'sam@mail.com',
-//           // cartsId: 'cdqdwir39232',
-//           userId: 'cm74hxlxy0009tsd4lzhwhwmv',
-//           order_id: data.order_id,
-//           // paymentsId: 'joewjfiewjfiwf',
-//           // courierId: 'wqeijeiqejei',
-//         },
-//         {
-//           headers: {
-//             // Authorization: `bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-
-//       const invoice_history_response = await axios.post(
-//         apiURL + '/invoice-history/create-invoice-history',
-//         {
-//           invoice_id: invoice_response.data.invoice_created.id,
-//         },
-//       );
-//       console.log('invoice_history created: ', invoice_history_response.data);
-
-//       await axios.post(
-//         `${apiURL}/payment/create-payment`,
-//         {
-//           bank: result.va_numbers?.[0]?.bank || 'unknown',
-//           gross_amount: Number(result.gross_amount),
-//           status_code: result.status_code,
-//           midtrans_transaction_id: result.transaction_id,
-//           invoicesId: invoice_response.data.invoice_updated.id,
-//         },
-//         {
-//           headers: {
-//             // Authorization: `Bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-//     }
-
-//     if (transactionStatus === 'settlement' && fraudStatus === 'accept') {
-//       console.log(`Payment for order ${data.order_id} successful.`);
-
-//       const updateInvoice = await axios.put(
-//         apiURL + '/invoice/update-invoice',
-//         {
-//           order_id: data.order_id,
-//           status: 'success',
-//         },
-//         {
-//           headers: {
-//             //   Authorization: `bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-
-//       // Create payment record
-//       await axios.post(
-//         `${apiURL}/payment/create-payment`,
-//         {
-//           bank: result.va_numbers?.[0]?.bank || 'unknown',
-//           gross_amount: Number(result.gross_amount),
-//           status_code: result.status_code,
-//           midtrans_transaction_id: result.transaction_id,
-//           invoicesId: updateInvoice.data.invoice_updated.id,
-//         },
-//         {
-//           headers: {
-//             // Authorization: `Bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-
-//       const invoice_history_response = await axios.put(
-//         apiURL + '/invoice-history/update-invoice-history',
-//         {
-//           invoice_id: updateInvoice.data.invoice_updated.id,
-//         },
-//         {
-//           headers: {
-//             // Authorization: `Bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-//       console.log('invoice_history created: ', invoice_history_response.data);
-//     } else if (transactionStatus === 'pending') {
-//       console.log(`Payment for order ${data.order_id} is pending.`);
-//       const service_charge = (800000 * 2 * 1) / 100;
-
-//       const invoice_response = await axios.post(
-//         apiURL + '/invoice/create-invoice',
-//         {
-//           status: 'pending',
-//           prices: 800000 * 2,
-//           service_charge: service_charge,
-//           receiver_city: 'jkt',
-//           receiver_province: 'jkt',
-//           receiver_subDistrict: 'jkt',
-//           receiver_district: 'jkt',
-//           receiver_phone: '092131322',
-//           receiver_name: 'sam',
-//           receiver_postalCode: '12440',
-//           receiver_detailAddress: 'ehiuewuhwuiehrweur',
-//           receiver_email: 'sam@mail.com',
-//           // cartsId: 'cdqdwir39232',
-//           userId: 'cm74hxlxy0009tsd4lzhwhwmv',
-//           order_id: data.order_id,
-//           // paymentsId: 'joewjfiewjfiwf',
-//           // courierId: 'wqeijeiqejei',
-//         },
-//         {
-//           headers: {
-//             // Authorization: `bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-
-//       const invoice_history_response = await axios.post(
-//         apiURL + '/invoice-history/create-invoice-history',
-//         {
-//           invoice_id: invoice_response.data.invoice_created.id,
-//         },
-//       );
-//       console.log('invoice_history created: ', invoice_history_response.data);
-//     } else if (
-//       transactionStatus === 'failure' ||
-//       transactionStatus === 'cancel' ||
-//       transactionStatus === 'expire'
-//     ) {
-//       console.log(`Payment for order ${data.order_id} failed.`);
-//       await axios.put(
-//         apiURL + '/invoice/update-invoice',
-//         {
-//           order_id: data.order_id,
-//           status: 'failed',
-//         },
-//         {
-//           headers: {
-//             //   Authorization: `bearer ${token}`,
-//             'Content-Type': 'application/json',
-//           },
-//         },
-//       );
-//     }
-
-//     res.status(200).json({ message: 'Webhook received' });
-//   } catch (error) {
-//     console.error('Error processing Midtrans webhook:', error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// }
--- src/routes/v1/order.route.ts
+++ src/routes/v1/order.route.ts
@@ -4,7 +4,7 @@ import { authentication } from '../../middlewares/authmiddleware';
 const orderRoute = express.Router();
 
 orderRoute.post('/add-order', orders.createOrder);
-orderRoute.get('/:id', orders.retrieveOrder);
 orderRoute.post('/confirm/:id', orders.confirmDraftOrder);
 orderRoute.post('/draft-order', orders.createDraftOrder);
+orderRoute.post('/', authentication, orders.retrieveOrder);
 export default orderRoute;
